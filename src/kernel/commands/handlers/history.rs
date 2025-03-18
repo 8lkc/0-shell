50bb5cf968600f1437f45a6456510b277471d4f9
8lkc <papebilalysow@gmail.com>
2025-03-18 16:23:20 +0000

    🛠️ Enhance history command to display the last 16 entries from the log file and improve argument handling

--- src/kernel/commands/handlers/history.rs
+++ src/kernel/commands/handlers/history.rs
@@ -7,15 +7,18 @@ pub struct History;
 impl CommandHandler for History {
     fn execute(args: &[String]) -> Result<(), std::io::Error> {
         if args.is_empty() {
-            let mut number: u32 = 1;
             let file = File::open("logs/history.log")?;
             let reader = io::BufReader::new(file);
-            for line in reader.lines() {
-                println!("{:>5}  {}", number, line?);
-                number += 1;
+
+            let lines: Vec<_> = reader.lines().collect::<Result<_, _>>()?;
+            let start = if lines.len() > 16 { lines.len() - 16 } else { 0 };
+
+            for (i, line) in lines.iter().skip(start).enumerate() {
+                println!("{:>5}  {}", start + i + 1, line);
             }
             return Ok(());
         }
+
         Err(io::Error::new(
             io::ErrorKind::InvalidInput, 
             format!("\x1b[1;3mhistory:\x1b[0m \x1b[38;5;{}mtoo many arguments\x1b[0m", rgb_to_ansi256(220, 45, 34))
